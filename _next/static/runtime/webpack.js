4384bf5c0125fccd8d7b5ce7f2f14db460e85dba
Hieu Phan <hieutrunglephan@gmail.com>
2020-06-12 01:24:54 +0700

    add post 2

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4af2f1ec9e8cb4bc4fe3";
+/******/ 	var hotCurrentHash = "577b4e983b24adbcb70c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/post/[slug].js.c4a741d02f89b04c3f3a.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static/development/pages/post/[slug].js",[
+/* 0 */,
+/* 1 */,
+/* 2 */
+false,
+/* 3 */
+/*!*************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fpost%2F%5Bslug%5D&absolutePagePath=%2FUsers%2Fhieudeptrai%2FMyProjects%2Fmyblog%2Fpages%2Fpost%2F%5Bslug%5D.js ***!
+  \*************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fpost%2F%5Bslug%5D&absolutePagePath=%2FUsers%2Fhieudeptrai%2FMyProjects%2Fmyblog%2Fpages%2Fpost%2F%5Bslug%5D.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fpost%2F%5Bslug%5D&absolutePagePath=%2FUsers%2Fhieudeptrai%2FMyProjects%2Fmyblog%2Fpages%2Fpost%2F%5Bslug%5D.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=[slug].js.c4a741d02f89b04c3f3a.hot-update.js.map
